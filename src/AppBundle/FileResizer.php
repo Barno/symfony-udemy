6f785bf26955394a2443934032014a39b00d713a
Alessio Barnini <alessio@ibuildings.it>
2016-06-18 17:19:54 +0200

    creato il nuovo servizio FileResizer

---
+++ src/AppBundle/FileResizer.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle;
+
+
+class FileResizer {
+
+  private $fileDir;
+  private $sizes;
+
+  /**
+   * FileResizer constructor.
+   * @param $fileDir
+   * @param $sizes
+   */
+  public function __construct($fileDir, $sizes) {
+    $this->fileDir = $fileDir;
+    $this->sizes = $sizes;
+  }
+
+  public function resizeImage($fileName) {
+    $name = strstr($fileName, '.', TRUE);
+    $extension = strstr($fileName, '.');
+    $fullPath = $this->fileDir . '/' . $fileName;
+
+    foreach ($this->sizes as $size) {
+
+      //prende entrambi le dimensioni
+      list($imgWidth, $imgHeight) = @getimagesize($fullPath);
+
+      $newFilePath = $this->fileDir . '/' . $name . '_' . $size['name'] . $extension;
+
+      //fa la divisione per entrambi e prende quello piu piccolo,
+      //questo per avere la giusta proporzione
+      $scale = max(
+              $size["max_height"] / $imgWidth,
+              $size["max_width"] / $imgHeight
+      );
+
+
+      if ($scale >= 1) {
+        if ($fullPath !== $newFilePath) {
+          return copy($fullPath, $newFilePath);
+        }
+        return;
+      }
+
+
+      //scala le immagini mantenedo le proporzioni
+      $newWidth = $imgWidth * $scale;
+      $newHeight = $imgHeight * $scale;
+
+
+      $newImg = @imagecreatetruecolor($newWidth, $newHeight);
+      switch (strtolower(substr(strrchr($extension, '.'), 1))) {
+        case 'jpg':
+        case 'jpeg':
+          $srcImg = @imagecreatefromjpeg($fullPath);
+          $writeImage = 'imagejpeg';
+          $imageQuality = 75;
+          break;
+        case 'gif':
+          @imagecolortransparent($newImg, @imagecolorallocate($newImg, 0, 0, 0));
+          $srcImg = @imagecreatefromgif($fullPath);
+          $writeImage = 'imagegif';
+          $imageQuality = NULL;
+          break;
+        case 'png':
+          @imagecolortransparent($newImg, @imagecolorallocate($newImg, 0, 0, 0));
+          @imagealphablending($newImg, FALSE);
+          @imagesavealpha($newImg, TRUE);
+          $srcImg = @imagecreatefrompng($fullPath);
+          $writeImage = 'imagepng';
+          $imageQuality = 9;
+          break;
+        default:
+          $srcImg = NULL;
+      }
+
+      //http://php.net/manual/en/function.imagecopyresampled.php
+      imagecopyresampled($newImg, $srcImg, 0, 0, 0, 0, $newWidth, $newHeight, $imgWidth, $imgHeight);
+
+      //imagejpeg() la funzione finale per salvare
+      $writeImage($newImg, $newFilePath, $imageQuality);
+
+      //Libero la memoria
+      @imagedestroy($srcImg);
+      @imagedestroy($newImg);
+    }
+  }
+
+
+}
