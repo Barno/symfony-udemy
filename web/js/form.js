0ec86e37370c55a970db4b8f4856c5eb27c6b3ac
Alessio Barnini <alessio@ibuildings.it>
2016-07-10 21:26:54 +0200

    aggiunta animazione

--- web/js/form.js
+++ web/js/form.js
@@ -3,21 +3,39 @@ $(document).ready(function () {
     $("#form-user").submit(function (e) {
         e.preventDefault();
         var form = $(this);
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize(),
-            success: function (msg) {
-                if(msg.status == 'OK'){
-                    $("#feedback").html(msg.template);
-                }else{
-                    $("#feedback").html(msg.template);
+
+        var btn = $(":submit", form);
+
+        if (!btn.is("[load]")) {
+            var prevHtml = btn.html();
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize(),
+                dataType: "json",
+                beforeSend: function () {
+                    btn.attr('load', true);
+                    btn.html('Loading');
+                },
+                success: function (msg) {
+                    if (msg.status == 'OK') {
+                        $("#form_user").empty();
+                        $("#feedback").empty();
+                        $(msg.template).hide().appendTo($("#form_user")).fadeIn(1000);
+                    } else {
+                        $("#feedback").html(msg.template);
+                    }
+                },
+                error: function () {
+                    alert("qualcosa è andato storto");
+                },
+                complete: function () {
+                    btn.html(prevHtml);
+                    btn.removeAttr('load');
                 }
-            },
-            error: function () {
-                //error
-            }
-        });
+            });
+        }
+
 
     });
 
