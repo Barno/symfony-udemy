62531c7f959bed01191daca0cbd653f42cc091ff
Alessio Barnini <alessio@ibuildings.it>
2016-07-10 15:35:36 +0200

    manage cookie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,20 +5,52 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/{nome}", name="homepage")
-     * @Method({"GET","POST"})
-     */
-    public function indexAction(Request $request,$nome)
-    {
-        throw $this->createNotFoundException('The product does not exist');
-        exit;
-        echo $request->attributes->get('nome');
-        $request->request->add(array('param' => 'POST'));
-        return $this->render('default/index.html.twig',['res' => $request]);
+class DefaultController extends Controller {
+
+  /**
+   * @Route("/", name="homepage")
+   * @Method({"GET"})
+   */
+  public function indexAction(Request $request) {
+    $cookie = new Cookie('nomeCookie','ValoreCookie',(time()+ 3600*27*7));
+    $response = new Response();
+    $response->headers->setCookie($cookie);
+    $response->setContent($this->renderView(':default:index.html.twig'));
+    return $response;
+  }
+
+  /**
+   * @Route("/getCookie", name="getCookie")
+   * @Method({"GET"})
+   */
+  public function getCookieAction(Request $request) {
+    $cookie = $request->cookies->get('nomeCookie');
+
+    if($request->cookies->has('nomeCookie')){
+      echo $cookie;
     }
+    //var_dump($request->cookies->keys());
+    $response = new Response();
+    $response->setContent($this->renderView(':default:index.html.twig'));
+    return $response;
+  }
+
+
+  /**
+   * @Route("/deleteCookie", name="deleteCookie")
+   * @Method({"GET"})
+   */
+  public function deleteCookieAction(Request $request) {
+    $response = new Response();
+    $response->headers->clearCookie('nomeCookie');
+    $response->setContent($this->renderView(':default:index.html.twig'));
+    return $response;
+  }
+
+
+
 }
