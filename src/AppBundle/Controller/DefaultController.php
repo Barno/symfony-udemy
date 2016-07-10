e5669e0a0d3a2813e6312ae34f114311abae8904
Alessio Barnini <alessio@ibuildings.it>
2016-07-10 16:02:22 +0200

    Json Response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,12 +7,25 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class DefaultController extends Controller {
+
+
+  /**
+   * @Route("/json", name="json")
+   * @Method({"GET"})
+   */
+  public function jsonAction(Request $request) {
+    $data = json_encode(['username' => 'alessio']);
+    $response = new Response($data);
+    $response->headers->set('content-type','application/json');
+    return $response;
+  }
 
   /**
    * @Route("/file", name="file")
