9c024b8025b57bf74119943b8aa83237ab84a62f
Alessio Barnini <alessio@ibuildings.it>
2016-07-10 18:18:45 +0200

    handle form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Utente;
 use AppBundle\Form\UtenteType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -17,6 +18,35 @@ class DefaultController extends Controller
     {
         $utente = new Utente();
         $form = $this->createForm(UtenteType::class,$utente);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $request->isXmlHttpRequest()){
+
+            $response = new JsonResponse();
+
+            if($form->isValid()){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($utente);
+                $em->flush();
+                $out['status'] = 'OK';
+                $out['msg'] = 'SUCCESS!';
+                $response->setData($out);
+                return $response;
+            }else{
+                $errors = $form->getErrors(true,true);
+
+                $err = array();
+                foreach ($errors as $error) {
+                    $err[] = $error->getMessage(). " <br/>";
+                }
+                $response->setData(array('status' => 'KO','errors' => $err));
+
+                return $response;
+            }
+        }
+
+
 
         return $this->render('default/index.html.twig',array(
             'form' => $form->createView()
--- web/js/form.js
+++ web/js/form.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    $("#form-user").submit(function (e) {
+        e.preventDefault();
+        var form = $(this);
+        $.ajax({
+            type: form.attr('method'),
+            url: form.attr('action'),
+            data: form.serialize(),
+            success: function (msg) {
+                if(msg.status == 'OK'){
+                    $("#feedback").html(msg.msg);
+                }else{
+                    $("#feedback").html(msg.errors);
+                }
+            },
+            error: function () {
+                //error
+            }
+        });
+
+    });
 
 });
 
