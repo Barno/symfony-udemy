0ff103a7263cc0f07c67cabdcca497e9dbe0dbb7
Alessio Barnini <alessio@ibuildings.it>
2016-07-24 20:34:48 +0200

    filtri array

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,17 +6,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        $date = new \DateTime("now");
-        $date = $date->format('d-m-Y H:i:s');
-        return $this->render('default/index.html.twig',array(
-            'date' => $date
-        ));
-    }
+class DefaultController extends Controller {
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function indexAction(Request $request) {
+    $date = new \DateTime("now");
+    $date = $date->format('d-m-Y H:i:s');
+
+    $users = ['cognone' => 'Barnini', 'nome' => 'Alessio', 'citta' => 'Livorno'];
+
+    return $this->render('default/index.html.twig', array(
+            'date' => $date,
+            'users' => $users
+    ));
+  }
 }
