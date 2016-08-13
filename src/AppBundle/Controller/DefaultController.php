23a4e45ea304794446877954ad04321f040aeff6
Alessio Barnini <alessio@ibuildings.it>
2016-08-13 20:21:59 +0200

    _format e default annotation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,18 +2,24 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/{nome}/{ruolo}.{_format}",name="homepage_stringa",defaults={"ruolo":"ADMIN","_format": "html"},requirements={"nome":"[a-z]{0,}","_format":"html|xml"})
+     * @Method({"GET","POST"})
+     */
     public function indexAction(Request $request,$nome,$ruolo,$_format)
     {
 
-        //var_dump($request->attributes->get('_route_params'));
-        //echo "<br>";
-        //echo $request->attributes->get('ruolo');
-        //echo "<br>";
+        var_dump($request->attributes->get('_route_params'));
+        echo "<br>";
+        echo $request->attributes->get('ruolo');
+        echo "<br>";
         //
         //echo $request->attributes->get('_controller');
         return $this->render('default/index.'.$_format.'.twig',array());
