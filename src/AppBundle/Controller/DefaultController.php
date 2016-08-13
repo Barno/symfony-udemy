098ba23b1453c73cf1f115d14c4021701967603d
Alessio Barnini <alessio@ibuildings.it>
2016-08-13 21:20:39 +0200

    prefix annotation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,11 +2,20 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/admin")
+ */
 class DefaultController extends Controller {
 
+  /**
+   * @Route("/{_locale}/{nome}/{ruolo}.{_format}",name="homepage_stringa",defaults={"ruolo":"ADMIN","_format": "html","title":"CIAO MONDO"},requirements={"nome":"[a-z]{0,}","_format":"html|xml","_locale":"%app.locales%"})
+   * @Method({"GET","POST"})
+   */
   public function indexAction(Request $request, $nome, $ruolo, $_format) {
 
     var_dump($request->attributes->get('_route_params'));
